3243c2e6a5c630b384891ecb8b1659b878e192a0
owennhorton <owen.n.horton@lawrence.edu>
2026-05-19 16:20:23 +0000

    Updated documentation for theme_academic

--- R/theme_academic.R
+++ R/theme_academic.R
@@ -7,7 +7,10 @@
 #' @export
 #'
 #' @examples
-#' theme_academic(base_size = 14)
+#' library(ggplot2)
+#' p <- ggplot(data = mtcars) + geom_point(aes(x = mpg, y = hp)) +
+#' labs(title = "plot_title")
+#' p + theme_academic()
 theme_academic <- function(base_size = 12)
 {
   ggplot2::theme_classic(base_size = base_size) +
